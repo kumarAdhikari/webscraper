a60a813c969a74b4245d2f266b1e95f6f4031097
kumar adhikari <kumaradhikari7@gmail.com>
2022-01-06 18:45:27 -0600

    second

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
 
+url = "https://www.newegg.com/abs-ali560/p/N82E16883360190?Item=N82E16883360190"
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text, "html.parser")
+
+price = doc.find_all(text="$")
+parent = price[0].parent
+finalprice = parent.find("strong")
+print(finalprice.text)
